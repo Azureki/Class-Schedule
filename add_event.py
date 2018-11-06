d3ab4f322493c666c3517d66a2dcd7537ba6d191
Azureki <fzureice@gmail.com>
2018-11-06 21:52:02 +0800

    测试：添加事件

---
+++ add_event.py
@@ -0,0 +1,53 @@
+from __future__ import print_function
+from googleapiclient.discovery import build
+from httplib2 import Http
+from oauth2client import file, client, tools
+
+# If modifying these scopes, delete the file token.json.
+SCOPES = 'https://www.googleapis.com/auth/calendar'
+
+
+def main():
+    """Shows basic usage of the Google Calendar API.
+    Prints the start and name of the next 10 events on the user's calendar.
+    """
+    store = file.Storage('token.json')
+    creds = store.get()
+    if not creds or creds.invalid:
+        flow = client.flow_from_clientsecrets('credentials.json', SCOPES)
+        creds = tools.run_flow(flow, store)
+    service = build('calendar', 'v3', http=creds.authorize(Http()))
+    event = {
+        'summary': 'Google I/O 2015',
+        'location': '800 Howard St., San Francisco, CA 94103',
+        'description': 'A chance to hear more about Google\'s developer products.',
+        'start': {
+            'dateTime': '2018-10-02T09:00:00+08:00',
+            # https://stackoverflow.com/questions/22526635/list-of-acceptable-google-calendar-api-time-zones
+            'timeZone': 'Asia/Shanghai',
+        },
+        'end': {
+            'dateTime': '2018-10-02T17:00:00+08:00',
+            'timeZone': 'Asia/Shanghai',
+        },
+        # 'recurrence': [
+        #     'RRULE:FREQ=DAILY;COUNT=2'
+        # ],
+        'attendees': [
+            {'email': '张菁，付岩@heu'},
+        ],
+        'reminders': {
+            'useDefault': False,
+            'overrides': [
+                # {'method': 'email', 'minutes': 24 * 60},
+                {'method': 'popup', 'minutes': 10},
+            ],
+        },
+    }
+
+    event = service.events().insert(calendarId='primary', body=event).execute()
+    print('Event created: %s' % (event.get('htmlLink')))
+
+
+if __name__ == '__main__':
+    main()
